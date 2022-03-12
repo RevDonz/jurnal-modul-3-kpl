a0b5e9221697f8025126ae23512d345cddef46b7
RevDonz <aprilio842@gmail.com>
2022-03-12 09:36:37 +0700

    First init

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace modul2_1302204051
+{
+    class MainClass
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
