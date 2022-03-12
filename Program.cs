751182bd2e7ef0c28c0e66cb8f6eef04428b25e3
RevDonz <aprilio842@gmail.com>
2022-03-12 10:34:48 +0700

    Menambahkan class PosisiKarakterGame

--- Program.cs
+++ Program.cs
@@ -6,7 +6,25 @@ namespace modul3_1302204051
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(KodeBuah.getKodeBuah(KodeBuah.NamaBuah.Apel));
+            Console.WriteLine(KodeBuah.getKodeBuah(KodeBuah.NamaBuah.Apel) + "\n");
+
+            PosisiKarakterGame posisiKarakter = new PosisiKarakterGame();
+            Console.WriteLine("Posisi saat ini : " + posisiKarakter.CurrentState);
+
+            posisiKarakter.activateTrigger(PosisiKarakterGame.Trigger.TombolS);
+            Console.WriteLine("Posisi saat ini : " + posisiKarakter.CurrentState);
+
+            posisiKarakter.activateTrigger(PosisiKarakterGame.Trigger.TombolW);
+            Console.WriteLine("Posisi saat ini : " + posisiKarakter.CurrentState);
+
+            posisiKarakter.activateTrigger(PosisiKarakterGame.Trigger.TombolW);
+            Console.WriteLine("Posisi saat ini : " + posisiKarakter.CurrentState);
+
+            posisiKarakter.activateTrigger(PosisiKarakterGame.Trigger.TombolX);
+            Console.WriteLine("Posisi saat ini : " + posisiKarakter.CurrentState);
+
+            posisiKarakter.activateTrigger(PosisiKarakterGame.Trigger.TombolS);
+            Console.WriteLine("Posisi saat ini : " + posisiKarakter.CurrentState);
         }
     }
 }
